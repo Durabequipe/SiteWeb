92ddddc9eee5065fd20c7d209ca9f58e4bf9cb68
Shammas44 <sebastien.traber@hotmail.com>
2023-04-18 11:27:01 +0200

    feat: incorporated a new font for icons

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -9,9 +9,22 @@ export class ButtonComponent {
   @Input() text = '';
   @Input() img?: string;
   @Input() link = '';
+  @Input() type: 'full' | 'square' | '' = '';
+  @Input() position?:
+    | 'top-left'
+    | 'top-right'
+    | 'bottom-left'
+    | 'bottom-right'
+    | '' = '';
 
-  getImgSrc() {
-    const imgSrc = this?.img ?? '';
-    return imgSrc ? `assets/${imgSrc}.svg` : '';
+  getImgClass() {
+    if (this.img) {
+      return `icon icon-${this.img}`;
+    }
+    return;
+  }
+
+  getClass() {
+    return `${this.type} ${this.position}`;
   }
 }
