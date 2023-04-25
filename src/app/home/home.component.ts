d8c2ac41f376709a3629fac5bd113c6a49e6e312
Shammas44 <sebastien.traber@hotmail.com>
2023-04-25 12:50:36 +0200

    fix: home video is now started once user has clicked on the page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,17 @@ import {
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  public videoPath = 'https://admin.durabilia.tarrit.ch/storage/videos/standalones/desktop/home.mp4'
-  constructor(private projectService: ProjectService) {}
+  public videoPath =
+    'https://admin.durabilia.tarrit.ch/storage/videos/standalones/desktop/home.mp4';
+  constructor(private projectService: ProjectService) {
+    document.body.addEventListener(
+      'click',
+      () => {
+        const video = document.querySelector('video');
+        video?.play()
+        console.log(video);
+      },
+      { once: true }
+    );
+  }
 }
